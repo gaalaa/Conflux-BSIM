062d07591694f49fbcab83753fc0fd9086f53a9e
gaalaa <gaalaa.zhou@gmail.com>
2023-12-13 16:38:11 +0800

    Add sturcture

--- contracts/interface.sol
+++ contracts/interface.sol
@@ -2,17 +2,26 @@
 pragma solidity ^0.8.0;
 
 interface ITokenWallet {
-    //function detectTokenTypes(address wallet) external view returns (TokenType[] memory);
+    //此方法返回指定钱包地址中检测到的所有代币类型
+    function detectTokenTypes(address wallet) external view returns (TokenType[] memory);
+    //此方法用于获取指定钱包中指定代币的余额
     function getTokenBalance(address wallet, address token) external view returns (uint256);
-    //function getNFTDetails(address wallet, address nftAddress, uint256 tokenId) external view returns (NFTDetails memory);
-    function editToken(address token, uint256 amount) external; // 具体参数取决于编辑操作的需求
+    //此方法用于获取指定钱包中特定NFT的详细信息
+    function getNFTDetails(address wallet, address nftAddress, uint256 tokenId) external view returns (NFTDetails memory);
+    function editToken(address token, uint256 amount) external;
 }
 
-// 可能需要的辅助数据结构
-//struct TokenType {
-    // ... 代币类型的详细信息
-//}
+struct TokenType {
+    address tokenAddress; // 代币合约地址
+    string tokenType; // 代币类型，例如"ERC-20", "ERC-721", "ERC-1155"
+    string name; // 代币名称
+    string symbol; // 代币符号
+}
 
-//struct NFTDetails {
-    // ... NFT的详细信息，例如图片URL和描述
-//}
+struct NFTDetails {
+    address nftAddress; // NFT合约地址
+    uint256 tokenId; // NFT的Token ID
+    string imageURL; // NFT的图像URL
+    string description; // NFT的描述
+}
+
