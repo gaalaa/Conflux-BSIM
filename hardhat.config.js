2f3773abe32801a78dec4deab1c2938c367fec51
gaalaa <gaalaa.zhou@gmail.com>
2023-12-13 17:59:32 +0800

    make some change

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    espaceTestnet: {
+      url: "https://evm.confluxrpc.com",
+      accounts: ["YOUR_PRIVATE_KEY"],
+    },
+    // 其他网络配置
+  },
 };
