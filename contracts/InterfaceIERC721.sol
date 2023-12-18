d8885fef4f950b2de3e238e2483e04a5c6ed14eb
GalaZhou <42603228+gaalaa@users.noreply.github.com>
2023-12-18 17:01:08 +0800

    Delete contracts/InterfaceIERC721.sol

--- contracts/InterfaceIERC721.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
-interface IERC721WithTokenURI is IERC721 {
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-}
