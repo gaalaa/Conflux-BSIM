a7a6c112107718f47a73acb1ae2d16989fac8dd2
gaalaa <gaalaa.zhou@gmail.com>
2023-12-19 16:57:16 +0800

    Add interface and function

--- contracts/IPriceInquire.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-//PriceInquire接口，资产查询
-interface IPriceInquire {
-    // 获取代币价格
-    function getPrice(address tokenAddress) external view returns (uint256);
-}
--- contracts/IWallet.sol
+++ contracts/IWallet.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+interface IWallet {
+
+    // register(add) token to user's wallet
+    function registerToken(address user, address token) external;
+
+    // inquire token's market price
+    function getTokenMarketPrice(address user, address token) external view returns (uint256);
+
+    // get paginated token balance
+    function getPaginatedTokenBalances(address user, uint start, uint limit) 
+    external view returns (address[] memory tokenAddresses, uint256[] memory balances);
+}
