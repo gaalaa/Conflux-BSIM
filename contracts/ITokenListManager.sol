64ece2ae0d2264ad6384447e881f2c78833f0d18
gaalaa <gaalaa.zhou@gmail.com>
2023-12-19 15:58:19 +0800

    Change comment and add some function

--- contracts/ITokenListManager.sol
+++ contracts/ITokenListManager.sol
@@ -1,19 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface ITokenList {
-    // add to whitelist
+interface ITokenListManager {
+
+    // add token to tokenlist
+    function addToken(address token) external;
+
+    // remove token from tokenlist
+    function removeToken(address token) external;
+
+    // add token to whitelist
     function addTokenToWhitelist(address token) external;
 
-    // remove from whitelist
+    // remove token from whitelist
     function removeTokenFromWhitelist(address token) external;
 
-    // add to blacklist
+    // add token to blacklist
     function addTokenToBlacklist(address token) external;
+
+    // remove token from blacklist
+    function removeTokenFromBlacklist(address token) external;
 
     // search token in whitelist
     function getWhitelistedTokens() external view returns (address[] memory);
 
-    // search toke in blacklist
+    // search token in blacklist
     function getBlacklistedTokens() external view returns (address[] memory);
 }
