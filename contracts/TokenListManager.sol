b6f968e15704f6dc4127e6e7219a79b6c094605e
gaalaa <gaalaa.zhou@gmail.com>
2023-12-26 18:01:04 +0800

    Import OZ lib

--- contracts/TokenListManager.sol
+++ contracts/TokenListManager.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.20;
 
 import {ITokenListManager} from "./ITokenListManager.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 contract TokenListManager is ITokenListManager {
 
