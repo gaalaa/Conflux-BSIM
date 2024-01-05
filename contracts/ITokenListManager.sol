35f0e6eb191d2a549a80f04db6738a9fefae99a9
gaalaa <gaalaa.zhou@gmail.com>
2024-01-05 16:26:32 +0800

    v1.2

--- contracts/ITokenListManager.sol
+++ contracts/ITokenListManager.sol
@@ -13,26 +13,28 @@ interface ITokenListManager is IERC165 {
     }
 
     // Add tokens to tokenlist(whitelist), batch supported
-    function addTokens(address[] calldata tokens, TokenType tokenType) external;
+    function addTokens(address[] calldata tokens) external;
 
     // Remove tokens from tokenlist(whitelist), batch supported
-    function removeTokens(address[] calldata tokens, TokenType tokenType) external;
+    function removeTokens(address[] calldata tokens) external;
 
     // Add tokens to blacklist, batch supported
-    function addBlacklistedTokens(address[] calldata tokens, TokenType tokenType) external;
+    function addBlacklistedTokens(address[] calldata tokens) external;
 
     // Remove tokens from blacklist, batch supported
-    function removeBlacklistedTokens(address[] calldata tokens, TokenType tokenType) external;
+    function removeBlacklistedTokens(address[] calldata tokens) external;
 
     // Get tokens in whitelist, paging supported
-    function getWhitelistedTokens(TokenType tokenType, uint256 offset, uint256 limit) external view returns (address[] memory, uint256);
+    function getWhitelistedTokens(uint256 offset, uint256 limit) 
+    external view returns (address[] memory whitelistedERC20Tokens, address[] memory whitelistedERC721Tokens, address[] memory whitelistedERC1155Tokens);
 
     // Get tokens in blacklist, paging supported
-    function getBlacklistedTokens(TokenType tokenType, uint256 offset, uint256 limit) external view returns (address[] memory, uint256);
+    function getBlacklistedTokens(uint256 offset, uint256 limit) 
+    external view returns (address[] memory blacklistedERC20Tokens, address[] memory blacklistedERC721Tokens, address[] memory blacklistedERC1155Tokens);
 
     // Determine if the token in whitelist
-    function isWhitelisted(address token, TokenType tokenType) external view returns (bool);
+    function isWhitelisted(address token) external view returns (bool);
 
     // Determine if the token in blacklist
-    function isBlacklisted(address token, TokenType tokenType) external view returns (bool);
+    function isBlacklisted(address token) external view returns (bool);
 }
